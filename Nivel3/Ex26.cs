0bd7783647552989a7fd9055ea8588da289a77f6
Lucinei Conceição de Lima <nei.androide@gmail.com>
2022-04-20 21:42:37 -0300

    Nivel3

---
+++ Nivel3/Ex26.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nivel3
+{
+    class Ex26
+    {
+        static void Main26(string[] args)
+        {
+            for(int i = 100; i>=1; i-- )
+            {
+                Console.WriteLine(i);
+            }
+                
+        }
+    }
+}
---
+++ Nivel3/Ex27.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nivel3
+{
+    class Ex27
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quantos, numeros quer saber o Fatorial \n");
+            int i = int.Parse(Console.ReadLine());
+            int[] num = new int[i];                                   
+            while(i > -1)
+            {
+
+            }
+            foreach (int numeros in num)
+            {
+
+            }
+        }
+    }
+}
