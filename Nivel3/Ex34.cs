f1fe32c849a673f02ba756749a614ee4911333e1
Lucinei Conceição de Lima <nei.androide@gmail.com>
2022-04-25 08:48:43 -0300

    Modificado o arquivo Ex34

--- Nivel3/Ex34.cs
+++ Nivel3/Ex34.cs
@@ -10,6 +10,22 @@ namespace Nivel3
     {
         static void Main(string[] args)
         {
+            List<Int32> Num;
+            Num = new List<Int32>();
+            int temp;
+
+            do
+            {
+                
+                Console.WriteLine("Digite um número para adicionar na a lista!");
+                temp = int.Parse(Console.ReadLine());
+                if (temp >= 0)
+                {
+                    Num.Add(temp);
+                }
+            }
+            while (temp < 0);
+
 
         }
     }
