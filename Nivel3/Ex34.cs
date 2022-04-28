c97d52ba04f7b31957446203cb014cb301e37582
Lucinei Conceição de Lima <nei.androide@gmail.com>
2022-04-28 08:54:04 -0300

    Concluido Ex34

--- Nivel3/Ex34.cs
+++ Nivel3/Ex34.cs
@@ -12,20 +12,33 @@ namespace Nivel3
         {
             List<Int32> Num;
             Num = new List<Int32>();
-            int temp;
+            int temp, max = 0, min = 0;
 
             do
             {
                 
-                Console.WriteLine("Digite um número para adicionar na a lista!");
+                Console.WriteLine("Digite um número positivo para adicionar na a lista!" +
+                    "ou um numero negativo para sair");
                 temp = int.Parse(Console.ReadLine());
+
                 if (temp >= 0)
                 {
                     Num.Add(temp);
                 }
             }
-            while (temp < 0);
+            while (temp > 0);
 
+            if(Num == null)
+            {
+                Console.WriteLine("Voce nao adicionou nenhum numero!");
+            }
+            else
+            {
+                max = Num.Max();
+                min = Num.Min();
+            }
+
+            Console.WriteLine($"O maior numero da lista é: {max}, e o menor numero é: {min}");
 
         }
     }
